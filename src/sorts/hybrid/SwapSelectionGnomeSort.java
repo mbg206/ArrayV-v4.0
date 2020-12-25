5b4dfd3c1e4cbb4e2877d8aa26d1652aae0a98de
mbg206 <66435380+mbg206@users.noreply.github.com>
2020-12-24 23:35:20 -0600

    Add files via upload

---
+++ src/sorts/hybrid/SwapSelectionGnomeSort.java
@@ -0,0 +1,74 @@
+package sorts.select;
+
+import main.ArrayVisualizer;
+import sorts.templates.Sort;
+
+/*
+ * 
+MIT License
+
+Copyright (c) 2019 w0rthy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ *
+ */
+
+final public class SwapSelectionGnomeSort extends Sort {  
+    public SwapSelectionGnomeSort(ArrayVisualizer arrayVisualizer) {
+        super(arrayVisualizer);
+        
+        this.setSortListName("Swap Selection Gnome");
+        this.setRunAllSortsName("Swap Selection Gnome Sort (Swap Selection + Gnome)");
+        this.setRunSortName("Swap Selection Gnome Sort");
+        this.setCategory("Hybrid Sorts");
+        this.setComparisonBased(true);
+        this.setBucketSort(false);
+        this.setRadixSort(false);
+        this.setUnreasonablySlow(false);
+        this.setUnreasonableLimit(0);
+        this.setBogoSort(false);
+    }
+    
+    @Override
+    public void runSort(int[] array, int length, int bucketCount) {
+        for (int i = 0; i < length / 2; i++) {
+            for (int j = i + 1; j < length; j++) {
+                Highlights.markArray(1, i);
+                Highlights.markArray(2, j);
+                Delays.sleep(0.01);
+                if (Reads.compareValues(array[j], array[i]) == -1){
+                    Writes.swap(array, i, j, 0.01, true, false);
+                }
+            }
+        }
+        for (int i = length / 2; i < length / 2 + length / 4; i++) {
+            Highlights.markArray(1, i);
+            Highlights.markArray(2, length - i);
+            Writes.swap(array, i, length - i + length / 2, 1, true, false);
+        }
+        for (int i = length - 1; i > 0; i--) {
+            if (Reads.compareValues(array[i + 1], array[i]) == -1){
+                    Writes.swap(array, i, i + 1, 0.03, true, false);
+                    if (i != length) {
+                        i = i + 2;
+                    }
+            }
+        }
+    }
+}
